cb9cddadb5226223524107ab11c834ab340b5e09
Tony Wilcock <tonywilcock80@gmail.com>
2023-07-11 16:41:44 +0100

    Initial Commit

---
+++ src/app/fonts.ts
@@ -0,0 +1,15 @@
+import { Inter, Abel, Waterfall } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'], display: 'swap' });
+
+export const abel = Abel({
+  subsets: ['latin'],
+  weight: '400',
+  display: 'swap',
+});
+
+export const waterfall = Waterfall({
+  subsets: ['latin'],
+  weight: '400',
+  display: 'swap',
+});
